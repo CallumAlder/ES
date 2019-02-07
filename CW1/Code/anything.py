8067e66ad3a44035a8f779a2ca58d9bc47a61ed5
aichinweze <aichinweze@gmail.com>
2019-02-07 12:01:48 +0000

    Dayum son
    Wher'd you find that

--- CW1/Code/anything.py
+++ CW1/Code/anything.py
@@ -1,3 +1,4 @@
+'''
 import si1145 as iig
 import json
 
@@ -14,8 +15,18 @@ vis = sensor.readVisible()
 # prox = sensor.readProx()
 
 data = {"IR": ir, "UV": uv, "Visible": vis}
+print(data)
 package = json.dumps(data)
 
 # do another thing and then push it
 # pusha T
 # pushed again gg
+'''
+
+import paho.mqtt as mqtt
+
+client = mqtt.Client(client_id="skad00sh")
+
+if client.connect("iot.eclipse.org", port=443) == 0:
+    mqtt.error_string(client.publish("hworld", "msg").rc)
+
