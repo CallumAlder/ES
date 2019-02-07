245e14bf7b27fdbdba40324c1945261575661012
aichinweze <aichinweze@gmail.com>
2019-02-07 10:43:36 +0000

    Mqtqtqttqtq
    Malaria

--- CW1/Code/anything.py
+++ CW1/Code/anything.py
@@ -1,8 +1,17 @@
 import si1145 as iig
+import json
 
 sensor = iig.SI1145()
 
 print(sensor)
 print("iig")
 print("iig")
+
+ir = sensor.readIR()
+uv = sensor.readUV()
+vis = sensor.readVisible()
+# prox = sensor.readProx()
+
+data = {"IR": ir, "UV": uv, "Visible": vis}
+package = json.dumps(data)
 
