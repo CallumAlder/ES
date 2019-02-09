9ec7d6ddfac2d37b0025516e3d165ec2c5845f21
CallumAlder <31134530+CallumAlder@users.noreply.github.com>
2019-02-09 14:35:17 +0000

    Delete midi_controller.sh

--- CW1/Midi/Raspberry-MIDI-Controller/midi_controller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# footswitch.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /
-cd home/pi/midi_controller
-sudo python3 midi_controller.py
-cd /
