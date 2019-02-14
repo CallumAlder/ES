62e7f06e47a0b943b64bae1287629eb163bc622b
CallumAlder <31134530+CallumAlder@users.noreply.github.com>
2019-02-14 01:40:16 +0000

    YO IT FUCKING WORKS GGGGG

--- CW1/midi2/test_mod.py
+++ CW1/midi2/test_mod.py
@@ -1,13 +1,9 @@
 import serial
 from time import sleep
-
 import pigpio
 
 # Set MIDI port
-#ser = serial.Serial('/dev/ttyAMA0', 
-#baudrate=38400, 
-#parity='N',
-#stopbits=0)  # Open serial at 38400bps
+ser = serial.Serial('/dev/ttyAMA0', baudrate=38400)  # Open serial at 38400bps
 
 #msg = str.encode('B11D3F)'
 #for ii in msg:
@@ -18,34 +14,33 @@ import pigpio
 
 #ser.write(outmidi)
 
-#msg = bytearray([27,63])
+#msg = bytearray([176,29,63])
 #ser.write(msg)
 
 
 
 
-pi1 = pigpio.pi()
-buttPin = 4
-pi1.set_mode(buttPin, pigpio.INPUT)
+#pi1 = pigpio.pi()
+#buttPin = 4
+#pi1.set_mode(buttPin, pigpio.INPUT)
 
 
-def buttonDown(gpio, level, tick):
-    print("DOWN")
+#def buttonDown(gpio, level, tick):
+#    print("DOWN")
+#    ser.write(msg)
     #outport.send(onmess)
 
 
-def buttonUp(gpio, level, tick):
-    print("UP")
+#def buttonUp(gpio, level, tick):
+#    print("UP")
     #outport.send(offmess)
 
 
-cb = pi1.callback(buttPin, pigpio.RISING_EDGE, buttonDown)
-cb2 = pi1.callback(buttPin, pigpio.FALLING_EDGE, buttonUp)
+#cb = pi1.callback(buttPin, pigpio.RISING_EDGE, buttonDown)
+#cb2 = pi1.callback(buttPin, pigpio.FALLING_EDGE, buttonUp)
 # Just loop and do nothing
 while True:
-    pass
-
-
-
-while 1:
-
+    ser.write(bytearray([176,29,63]))
+    sleep(2)
+    ser.write(bytearray([176,29,31]))
+    sleep(2)
