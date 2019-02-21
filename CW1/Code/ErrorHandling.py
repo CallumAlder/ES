ba2e8dea0b9ef96a4298b37e1116e45bf280d022
Callum Alder <callumalder@live.com>
2019-02-21 00:50:42 +0000

    AccelConnection

--- CW1/Code/ErrorHandling.py
+++ CW1/Code/ErrorHandling.py
@@ -1,23 +1,45 @@
+import logging
+
 
 # Parent class to handle bespoke errors
 class ErrorHandler(Exception):
     pass
 
+
 # Child classes for bespoke errors
 class AccelConnection(ErrorHandler):
-    pass
+    def __init__(self, expression, message):
+
+        if expression == None:
+            self.expression = ""
+        else:
+            self.expression = expression
+
+        if message == None:
+            self.message = "could not establish connection with GyroScope, please check wiring"
+        else:
+            self.message = message
+
 
 class IRConnection(ErrorHandler):
     pass
 
+
 class IRIOError(ErrorHandler):
     pass
+
 
 class BrokerConnection(ErrorHandler):
     pass
 
+
 class MIDIConnection(ErrorHandler):
     pass
 
+
 class MutexError(ErrorHandler):
+    pass
+
+
+class UndefinedSate(ErrorHandler):
     pass
